15d8c72ea6a0fa115049fee2975978fe72ced320
dilshan.d3 <dilshan72404@gmail.com>
2024-10-22 11:26:26 +0530

    pushing on hosting for github

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here!</h2>
+        <p>Choose from a divers menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to statisfy your craving and elevate your dinig experience, one delicius meal at a time. </p>
+         <button>View menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
